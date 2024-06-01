fa5576d2cc7ff161efb39f3d7da5b57b93bf7ba3
Matthieu <matthieu.cottet@outlook.com>
2024-06-01 19:31:31 +0200

    Improved Camera Functions Flipped image and added Autofocus

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 from picamera2 import Picamera2, Preview
+from libcamera import Transform, controls
 import time
 
 
@@ -7,16 +8,23 @@ camera = Picamera2()
 
 def init():
     print("Starting camera...")
-    camera_config = camera.create_still_configuration()
+    transform = Transform(vflip = True, hflip = True) #flipping image vertically and horizontally
+    camera_config = camera.create_still_configuration(transform=transform)
     camera.configure(camera_config)
-    camera.start_preview(Preview.NULL)
+    camera.start_preview(Preview.NULL) 
     camera.start()
-    time.sleep(2)
+    time.sleep(2) 
 
 
 def take_picture(filename="picture"):
     print("Taking picture...")
+    camera.autofocus_cycle()
     camera.capture_file(f"pictures/{filename}.jpg")
+
+
+def stop():
+    camera.stop()
+
 
 if __name__ == "__main__":
     init()
