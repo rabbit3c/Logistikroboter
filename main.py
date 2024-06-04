ee585a91fa5b02f7f5d55c53f2922ff405b62290
Matthieu <matthieu.cottet@outlook.com>
2024-06-04 16:24:43 +0200

    Multithreading initializing

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from navigate import *
 import robot
 import camera
 from loop import loop
+from threading import Thread
 
 
 def main():
     print("\033[32mStarting...\033[0m\n")
 
-    robot.init()
-    camera.init()
+    init()
 
     # start_point = input_tuple("Enter start point x, y. \nRecommended start points are (1, 6), (1, 5), (1, 3), (1, 2): ")
     start_point = (1, 2)
@@ -17,6 +17,18 @@ def main():
     print()
 
     loop(start_point, direction)
+
+
+def init():
+    #initialize camera and robot in seperate threads to decrease startup time
+    camera_thread = Thread(target=camera.init)
+    robot_thread = Thread(target=robot.init)
+
+    camera_thread.start()
+    robot_thread.start()
+
+    camera_thread.join()
+    robot_thread.join()
 
 
 def input_tuple(prompt):
