8626129968f171ced648427cca07f854231f8adc
Matthieu <matthieu.cottet@outlook.com>
2024-06-05 17:35:10 +0200

    Website can show state of robot

--- web_server.py
+++ web_server.py
@@ -13,20 +13,22 @@ main_thread = None
 @app.route("/")
 def home():
     return "<p>Logistikroboter Web Server<p>"
-
      
+
 @app.route('/control', methods = ['POST'])
 def control():
     global main_thread
 
-    action = request.form['action']
+    command = request.form['command']
 
-    match action:
+    match command:
         case "start":
             stop_event.clear()
-
+            
             main_thread = Thread(target = main) # run main in seperate thread to be able to stop it
             main_thread.start()
+
+            return "started"
         
         case "stop":
             stop_event.set() # signal the main function to stop
@@ -34,10 +36,11 @@ def control():
             if main_thread is not None:
                 main_thread.join() # wait for the thread to finish
 
-    return 'Aktion ausgeführt: ' + action
+            return "stopped"
+        
+    return 'Aktion konnte nicht ausgeführt werden: ' + command
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
 # running on http://raspberrypi.local:5000 or in my case http://192.168.1.32:5000
